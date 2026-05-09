1afc6a07b0b4d2bb9d3d7f254dbabec656460609
shwetakeshrwani92-max <shwetakeshrwani92@gmail.com>
2026-05-09 23:26:26 +0530

    Add files via upload

---
+++ FindFirstOccurence.java
@@ -0,0 +1,24 @@
+public class FindFirstOccurence {
+    public int strStr(String haystack, String needle) {
+         int n = haystack.length();
+        int m = needle.length();
+        for (int i = 0; i <= n - m; i++) {
+            int j = 0;
+            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+            if (j == m) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+    String haystack = "sadbutsad";
+    String needle = "sad";   
+    FindFirstOccurence obj=new FindFirstOccurence();
+    int result = obj.strStr(haystack, needle);
+    System.out.println(result);   
+    }
+}  
+
