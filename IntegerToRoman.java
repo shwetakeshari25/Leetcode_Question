7300315ac6cfa08397f891e6c878da15ba7e5519
shwetakeshrwani92-max <shwetakeshrwani92@gmail.com>
2026-05-19 19:10:57 +0530

    Add files via upload

---
+++ IntegerToRoman.java
@@ -0,0 +1,18 @@
+class IntegerToRoman {
+    public String intToRoman(int num) {
+    int value[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    String symbol[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    StringBuilder ans=new StringBuilder();
+    for(int i=0;i<value.length;i++){
+        while(num>=value[i]){
+            ans.append(symbol[i]);
+            num=num-value[i];
+        }
+    }
+    return ans.toString();
+    }
+    public static void main(String args[]){
+        IntegerToRoman obj =new IntegerToRoman();
+        System.out.println(obj.intToRoman(3749));
+    }
+}
